8bd4a8965875eaf688dfa16cde55f724107624f4
Bill Lorensen <bill.lorensen@gmail.com>
2015-07-05 08:13:59 -0400

    ENH: Remote module setup
    This file must be copied to ITK/Modules/Remote

---
+++ PoissonReconstruction.remote.cmake
@@ -0,0 +1,10 @@
+#
+# Poisson Reconstruction
+#
+
+itk_fetch_module(PoissonReconstruction
+  "Poisson Surface reconstruction from unorganized points"
+  GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
+  # July 5, 2015 - first working as a remote module
+  GIT_TAG 79a477b0dcff4a0d985845d517c4ae065b9892ba
+  )
